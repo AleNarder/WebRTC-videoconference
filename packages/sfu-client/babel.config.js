ad2ac3a2147309bfae257f9a7e61dcd974df306a
Alessio Narder <alessio.narder@gmail.com>
2021-01-17 17:12:30 +0100

    chore(sfu-client): added nullish coalescing babel plugin

--- packages/sfu-client/babel.config.js
+++ packages/sfu-client/babel.config.js
@@ -2,5 +2,8 @@ const devPresets = ['@vue/babel-preset-app']
 const buildPresets = ['@babel/preset-env', '@babel/preset-typescript']
 module.exports = {
   presets: process.env.NODE_ENV === 'development' ? devPresets : buildPresets,
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
 }
